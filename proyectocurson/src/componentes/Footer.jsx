40b1bd0a68b1d2b799cec12477b1e143276247e6
Florencia Pistan Esparza <flor.pistan.esparza@gmail.com>
2020-11-17 16:15:52 -0300

    creando componente footer

---
+++ proyectocurson/src/componentes/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import '../css/styles.css'
+import {IconContext} from 'react-icons';
+import { FaDiscord, FaTwitter, FaFacebook, FaInstagram } from 'react-icons/fa'; 
+
+export default function Footer() {
+    return (
+        <div className="container-fluid bg-negro p-3 fixed-bottom">
+            <div className="row">
+                <div className="col-12 col-sm-6 text-white text-center">
+                    <ul className="p-0">
+                        <li>Inicio</li>
+                        <li>Cursos</li>
+                        <li>Categorías</li>
+                        <li>Publica tu curso</li>
+                        <li>Reviews</li>
+                    </ul>
+                </div>
+                <IconContext.Provider value={{ size:"2em" }}>
+                <div className="col-12 col-sm-6 text-white text-center m-0 pt-4">
+                    <FaDiscord /> 
+                    <FaTwitter  /> 
+                    <FaFacebook />
+                    <FaInstagram />
+                </div>
+                </IconContext.Provider>
+            </div>
+            <div className="row text-center text-white m-0 p-2">
+                <p className="mx-auto mt-2">2020 &copy; Diseñado y desarrolado por estudiantes de RollingCode School</p>
+            </div>            
+        </div>
+    )
+}
--- proyectocurson/src/index.js
+++ proyectocurson/src/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Footer from './componentes/Footer.jsx';
+import 'bootstrap/dist/css/bootstrap.css';
 
+const container = document.getElementById('root');
+
+ReactDOM.render(
+    <Footer />,
+    container
+);
