1f5420311949996cd728c8f014e317e6b2dcc87f
Florencia Pistan Esparza <flor.pistan.esparza@gmail.com>
2020-11-19 20:47:50 -0300

    agregando margin a los iconos

--- proyectocurson/src/index.js
+++ proyectocurson/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Footer from './componentes/Footer.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 
+
 const container = document.getElementById('root');
 
 ReactDOM.render(
