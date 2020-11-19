990ef0b498f74fc86889d82511ba9271e189c8c2
Belen Neme <belenneme1@gmail.com>
2020-11-18 23:48:13 -0300

    Cambiando db.json por data en el Componente

--- proyectocurson/src/index.js
+++ proyectocurson/src/index.js
@@ -1,1 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Reviews from "./component/Reviews";
+import "bootstrap/dist/css/bootstrap.css";
 
+
+const container = document.getElementById("root");
+
+// ReactDOM.render(_que_,_donde_)
+ReactDOM.render(<Reviews />, container);
