b67a0d8f4c15ba944451168ba34d69aab8a1237e
Gabriel Moreira <gabrielmoreiraginarte@gmail.com>
2020-11-19 16:14:35 -0300

    empezando 'todos los cursos'

---
+++ proyectocurson/src/component/Cursos.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+
+export default function Cursos() {
+    return (
+        <div className="container">
+            <h1>TODOS LOS CURSOS</h1>
+            <div className="row">
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card-curso m-1">
+                        <img src="https://picsum.photos/300/300/" className="card-img-top img-fluid p-1" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <span class="badge badge-primary">Primary</span>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn-primary-curso float-right">Ver Más</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card-curso m-1">
+                        <img src="https://picsum.photos/300/300/" className="card-img-top img-fluid p-1" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn-primary-curso float-right">Ver Más</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card-curso m-1">
+                        <img src="https://picsum.photos/300/300/" className="card-img-top img-fluid p-1" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn-primary-curso float-right">Ver Más</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card-curso m-1">
+                        <img src="https://picsum.photos/300/300/" className="card-img-top img-fluid p-1" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn-primary-curso float-right">Ver Más</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card-curso m-1">
+                        <img src="https://picsum.photos/300/300/" className="card-img-top img-fluid p-1" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn-primary-curso float-right">Ver Más</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card-curso m-1">
+                        <img src="https://picsum.photos/300/300/" className="card-img-top img-fluid p-1" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn-primary-curso float-right">Ver Más</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- proyectocurson/src/index.js
+++ proyectocurson/src/index.js
@@ -1,1 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Cursos from './component/Cursos';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
+ReactDOM.render(<Cursos />, document.getElementById('root'));
