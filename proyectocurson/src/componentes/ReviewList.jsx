b030ef6e29a17c8dce38f5d5b4403a26f2d0acb8
Belen Neme <belenneme1@gmail.com>
2020-11-23 15:20:52 -0300

    Agregando seccion [Nueva Review]

--- proyectocurson/src/componentes/ReviewList.jsx
+++ proyectocurson/src/componentes/ReviewList.jsx
@@ -6,7 +6,7 @@ export default function ReviewList({ data }) {
     <>
         {data.map((review) => {
           return (
-                <div className="col-md-6 mt-3">
+                <div key= {review.id} className="col-md-6 mt-3">
                     <div className="card mb-3 card-multicolor">
                         <div className="row no-gutters d-flex justify-content-center">
                             <div className="col-10 col-md-4 d-flex justify-content-center align-items-center">
@@ -14,9 +14,9 @@ export default function ReviewList({ data }) {
                             </div>
                             <div className="col-10 col-md-8">
                                 <div className="card-body">
-                                    <h5 className="card-title text-center mt-2">{review.cursoName}</h5>
+                                    <h5 className="card-title text-center mt-2">{review.curso}</h5>
                                     <p className="card-text">{review.comentario}</p>
-                                    <p className="card-text"><small className="color-rosa">{review.userName}</small></p>
+                                    <p className="card-text"><small className="color-rosa">{review.user}</small></p>
                                     <p className="card-text"><small className="text-muted">{review.fecha}</small></p>
                                     <p className="card-text"><small className="color-rosa">{review.calificacion}</small></p>
                                 </div>
