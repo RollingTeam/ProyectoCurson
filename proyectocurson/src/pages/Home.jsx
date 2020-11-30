d5dfebd69296178e3fe5a22966f3b15a04c6b1de
Mary Bosch <marybosch.99@gmail.com>
2020-11-30 11:29:25 -0300

    Publicar curso y paso a paso

--- proyectocurson/src/pages/Home.jsx
+++ proyectocurson/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Footer from "../componentes/Footer"
 import Reviews from "../componentes/Reviews"
 import "bootstrap/dist/css/bootstrap.css";
 import Navbar from "../componentes/Navbar";
+import PublicarCurso from "../componentes/PublicarCurso"
 
 export default function Home() {
 
@@ -10,6 +11,7 @@ export default function Home() {
     <>
       <Navbar />
       <Reviews />
+      <PublicarCurso />
       <Footer />
     </>
   );
