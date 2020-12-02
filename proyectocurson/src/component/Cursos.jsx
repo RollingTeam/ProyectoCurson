cb4201547127545b0904a37ac3654a986ca660fe
Gabriel Moreira <gabrielmoreiraginarte@gmail.com>
2020-12-02 18:09:52 -0300

    terminado el modal

--- proyectocurson/src/component/Cursos.jsx
+++ proyectocurson/src/component/Cursos.jsx
@@ -15,12 +15,12 @@ export default function Cursos() {
     }
     return (
         <div className="col-12 col-md-6 col-lg-4">
-            <div className="card-curso m-1">
-                <img src="https://picsum.photos/400/200/" className="card-img-top img-fluid p-1" alt="..." />
+            <div className="card-curso m-2">
+                <img src="https://picsum.photos/400/200/" className="card-img-top img-fluid" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title">Este es un curso dedico a la manufactura de telas rupestres siempre y cuando</h5>
+                    <h5 className="card-title"><strong>Este es un curso dedico a la manufactura de telas rupestres siempre y cuando</strong></h5>
                     <span class="badge badge-primary">Primary</span>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">¿Siempre has querido aprender a dibujar pero no sabes por dónde empezar? En este curso el ilustrador y educador Puño, te enseñará las claves para convertirte en dibujante desde cualquier nivel y a cualquier edad a través de una aproximación</p>
                     <button type="button" className="btn-primary-curso float-right" onClick={handleOpenModal}>Ver Más</button>
 
                     {openModal &&
--- proyectocurson/src/component/ModalVerMasCursos.jsx
+++ proyectocurson/src/component/ModalVerMasCursos.jsx
@@ -7,18 +7,24 @@ export default function ModalVerMasCursos(props) {
             <div className="Modal__container">
                 <div className="modal-dialog">
                     <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title">Modal title</h5>
+                        <div className="modal-header p-1">
+                            <h5 className="modal-title p-2"><strong>Curso de Mecánica Avanzada</strong></h5>
                             <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={props.handleCloseModal}>
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <div className="modal-body">
-                            <p>Modal body text goes here.</p>
+                            <img src="https://picsum.photos/400/200/" class="card-img-top img-fluid" alt="..."></img>
+                            <span class="badge badge-warning categorias">categorias</span>
+                            <span class="badge badge-pill badge-info cupos"> Lugares Disponibles</span>
+                            <div className="text-center mt-1">
+                                <p>¿Siempre has querido aprender a dibujar pero no sabes por dónde empezar? En este curso el ilustrador y educador Puño, te enseñará las claves para convertirte en dibujante desde cualquier nivel y a cualquier edad a través de una aproximación</p>
+                            </div>
+                            <div class="text-center">Contacto:<a href="link">https://www.domestika.org/es</a></div>
                         </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={props.handleCloseModal}>Close</button>
-                            <button type="button" className="btn btn-primary">Save changes</button>
+                        <div className="modal-footer justify-content-center">
+                            <button type="button" className="btn-primary-curso" data-dismiss="modal" onClick={props.handleCloseModal}>Inscribirse</button>
+                            <button type="button" className="btn-primary-curso">Agregar a mis favoritos</button>
                         </div>
                     </div>
                 </div>
