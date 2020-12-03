5054327a121d5f89d7653228c2f56507658a9cab
Florencia Pistan Esparza <flor.pistan.esparza@gmail.com>
2020-12-03 19:47:19 -0300

    agregando items

--- proyectocurson/src/pages/Home.jsx
+++ proyectocurson/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "../componentes/Footer"
+import NavbarAdminHome from "../componentes/NavbarAdminHome";
 import Reviews from "../componentes/Reviews"
 
 export default function Home() {
 
   return (
     <>
+      <NavbarAdminHome />
       <Reviews />
       <Footer />
     </>
