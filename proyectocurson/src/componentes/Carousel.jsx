f82b5966132d617fd0c5aecc33cc2b133048c601
Lean Imohff <imohfflean@gmail.com>
2020-12-04 18:38:43 -0300

    subiendo carousel

---
+++ proyectocurson/src/componentes/Carousel.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default function Carousel() {
+    return (
+        <div className="container-fluid">
+            <Carousel>
+  <Carousel.Item interval={1000}>
+    <img
+      className="d-block w-100"
+      src="https://picsum.photos/200/300"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item interval={500}>
+    <img
+      className="d-block w-100"
+      src="https://picsum.photos/200/300"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://picsum.photos/200/300"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+        </div>
+    )
+}
---
+++ proyectocurson/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Carousel from "../componentes/Carousel"
+import "bootstrap/dist/css/bootstrap.css"
+export default function Home() {
+    return (
+        <>
+            <Carousel />
+        </>
+    )
+}
