169e8ee9cc77590b7626d87f2631523b352f8001
Belen Neme <belenneme1@gmail.com>
2020-12-18 00:02:37 -0300

    Agragndo POST Y GET para cursos desde admin

--- proyectocurson/src/pages/Home.jsx
+++ proyectocurson/src/pages/Home.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import Footer from "../componentes/Footer"
 import Reviews from "../componentes/Reviews"
 import Navbar from "../componentes/Navbar";
-// import CursosDestacados from '../componentes/CursosDestacados'
-// import Categorias from "../componentes/Categorias";
+import CursosDestacados from '../componentes/CursosDestacados'
+import Categorias from "../componentes/Categorias";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      {/* <CursosDestacados />
-      <Categorias /> */}
+      <CursosDestacados />
+      <Categorias />
       <Reviews />
       <Footer />
     </>
