6f6dd58d6160b284d83bb57d1fb2356f3fc949cc
Florencia Pistán Esparza <flor.pistan.esparza@gmail.com>
2020-12-27 02:20:21 -0300

    Update Home.jsx

--- proyectocurson/src/pages/Home.jsx
+++ proyectocurson/src/pages/Home.jsx
@@ -3,14 +3,14 @@ import Footer from "../componentes/Footer";
 import BarraDeNavegacion from "../componentes/BarraDeNavegacion";
 import PublicarCurso from '../componentes/PublicarCurso'
 import Categorias from "../componentes/Categorias";
-import Cursos from '../componentes/Cursos'
+import TodosCursos from '../componentes/TodosCursos'
 
 export default function Home() {
   return (
     <>
       <BarraDeNavegacion />
       <Categorias />
-      <Cursos />
+      <TodosCursos />
       <PublicarCurso />
       <Footer />
     </>
