d7b91b314c283c66cbb50572b9e266be796c068b
Florencia Pistán Esparza <flor.pistan.esparza@gmail.com>
2021-01-15 18:29:47 -0300

    reemplazando texto por logo curson en jumbotron para mobile

--- src/componentes/WelcomeMobile.jsx
+++ src/componentes/WelcomeMobile.jsx
@@ -3,13 +3,14 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 import Button from 'react-bootstrap/Button'
 import '../css/Carousel.css'
 import {Link} from 'react-router-dom'
+import Logo from '../img/logo-navbar.png'
 
 export default function WelcomeMobile() {
     return (
         <div className="d-block d-sm-none">
             <Jumbotron className="bg-cover text-white mt-1">
                 <div className="container">
-                    <h1 className="display-4">CursOn</h1>
+                    <img src={Logo} alt="logo curson" className="logo-mobile mb-2"/>
                     <p className="lead">Somos una plataforma online con el propósito de proporcionar un espacio dedicado a estudiantes y mentores.
                     Podrás encontrar gran variedad de cursos de diversas categorías, desde arte y música hasta ciencia y programación.</p>
                     <hr className="my-4"/>
