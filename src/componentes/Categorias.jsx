52261acaf1609d973c59cf360f7c78986a12c44a
Florencia Pistán Esparza <flor.pistan.esparza@gmail.com>
2021-02-23 20:05:34 -0300

    agregando titulo a la sección categorías

--- src/componentes/Categorias.jsx
+++ src/componentes/Categorias.jsx
@@ -5,6 +5,7 @@ import CarrouselCategorias from "./CarrouselCategorias";
 export default function Categorias() {
   return (
     <div className="d-none d-xl-block" id="categorias">
+      <h3 className="text-center">Categorías</h3>
       <NavCategorias />
       <CarrouselCategorias />
     </div>
